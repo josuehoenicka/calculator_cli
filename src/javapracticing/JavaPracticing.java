50bdb85da7b24e5185b3786132ced4f4236e416e
Josué Hoenicka <josuee.vzla@gmail.com>
2022-09-23 12:14:28 -0300

    variables, whilte, for

--- src/javapracticing/JavaPracticing.java
+++ src/javapracticing/JavaPracticing.java
@@ -3,7 +3,27 @@ package javapracticing;
 public class JavaPracticing {
 
     public static void main(String[] args) {
-        System.out.println("Hola mundo!");
-    }
+        /* variables*/
+        int numOne = 1;
+        int numTwo = 2;
+        
+        /* alternative conditional */
+        if (numOne > numTwo) {
+            System.out.println(numOne + " es mayor a " + numTwo);
+        } else {
+            System.out.println(numTwo + " es mayor a " + numOne);
+        }
+        
+        /* while */
+        while(numOne < numTwo) {
+        System.out.println(numOne + " es menor a " + numTwo);
+        numOne++;
+        }
+        
+        /* for */
+        for (int i = 1; i < 3; i++) {
+            System.out.println("i es " + i);
+        }
     
+    }
 }
