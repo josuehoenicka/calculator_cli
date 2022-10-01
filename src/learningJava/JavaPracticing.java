32848ee61d92ebd41c33b8998f1b86f73646b895
Josué Hoenicka <josuee.vzla@gmail.com>
2022-10-01 19:41:03 -0300

    fix structure: add input scanners

--- src/learningJava/JavaPracticing.java
+++ src/learningJava/JavaPracticing.java
@@ -1,77 +1,23 @@
 package learningJava;
 
+import java.math.BigDecimal;
+import java.util.Scanner;
+
 public class JavaPracticing {
-
+    
     public static void main(String[] args) {
-
-        /* variables*/
         
-        int numOne = 1;
-        int numTwo = 2;
-        
-        /* alternative conditional */
-        
-        if (numOne > numTwo) {
-            System.out.println(numOne + " es mayor a " + numTwo);
-        } else {
-            System.out.println(numTwo + " es mayor a " + numOne);
+        try ( 
+            // creates an object of Scanner
+            Scanner input = new Scanner(System.in)) {
+            System.out.print("Enter your name: ");
+            String name = input.nextLine();
+            System.out.println("Welcome " + name);
+            System.out.print("Enter your age: ");
+            BigDecimal value = input.nextBigDecimal();
+            System.out.println(name + "'s age is " + value); 
+            System.out.print("\nHave a nice day " + name + "!\n\n\n"); 
         }
         
-        /* while */
-        
-        while(numOne < numTwo) {
-        System.out.println(numOne + " es menor a " + numTwo);
-        numOne++;
-        }
-        
-        /* for */
-        
-        for (int i = 1; i < 6; i++) {
-            System.out.println("i es igual a " + i);
-        }
-        
-        /* references */
-        
-        cat();
-        System.out.println("2 + 3 = " + plus(2,3));
-        System.out.println("2 - 3 = " + minus(2, 3));
-    
-        /* Instaces */
-        
-        People somebody = new People();
-        System.out.println("\n");
-        somebody.changeName("Josue");
-        somebody.happy();
-        somebody.speak();
-        System.out.println("\n");
-        somebody.changeName("Gabriel");
-        somebody.sad();
-        somebody.speak();
-      
     }
-    
-    /* methods */
-    
-    public static void cat(){
-       String garfield = "Garfield";
-       String chiara = "Chiara";
-       String felix = "Felix";
-       String speak = " say: ";
-       String cat = "Miau";
-       String dog = "Guau Guau";
-       
-       System.out.println(garfield + speak + cat);
-       System.out.println(chiara + speak + cat);
-       System.out.println(felix + speak + dog);
-         
-    }
-    
-    public static int plus(int numOne, int numTwo){
-        return numOne + numTwo;
-    }
-    
-    public static int minus(int numOne, int numTwo){
-        return numOne - numTwo;
-    }
-    
 }
